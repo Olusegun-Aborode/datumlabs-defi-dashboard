b83f6170d23b9a1cc981bf7f0481704d7ba4e899
Olusegun <110054818+Olusegun-Aborode@users.noreply.github.com>
2026-02-23 13:57:24 +0000

    chore: clean up leftover testing scripts from the explorer root

--- clear-db.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-async function main() {
-  const db = new PrismaClient();
-  await db.liquidationEvent.deleteMany({});
-  await db.walletPosition.deleteMany({});
-  console.log("Cleared old zero-dollar records");
-  await db.$disconnect();
-}
-main();
